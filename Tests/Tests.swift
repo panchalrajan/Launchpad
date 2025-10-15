b467d267ec7784f5266e3015a2af7d87139d0bde
papp.kristof <papp.kristof@ma-coding.com>
2025-10-15 17:25:05 +0200

    Tests

---
+++ Tests/Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Tests.swift
+//  Tests
+//
+//  Created by Papp Kristóf on 2025. 10. 15..
+//
+
+import Testing
+
+struct Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
