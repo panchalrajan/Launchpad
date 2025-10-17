b5272510d09f99d0f2a4d3935ec5a7c550678a35
Rajan Panchal <panchal.rajan2k19@gmail.com>
2025-10-17 18:29:01 +0530

    Fixed Focus Change break Search issue

--- Launchpad/Components/WindowAccessor.swift
+++ Launchpad/Components/WindowAccessor.swift
@@ -4,14 +4,32 @@ struct WindowAccessor: NSViewRepresentable {
    func makeNSView(context: Context) -> NSView {
       let view = NSView()
       DispatchQueue.main.async {
-         guard let window = view.window else { return }
-         window.titleVisibility = .hidden
-         window.styleMask.remove([.resizable, .titled])
-         window.level = .floating
-         window.setFrame(NSScreen.main?.frame ?? .zero, display: true)
+         guard let currentWindow = view.window else { return }
+
+         let newWindow = ResponsiveWindow(
+            contentRect: currentWindow.frame,
+            styleMask: currentWindow.styleMask,
+            backing: .buffered,
+            defer: false
+         )
+
+         newWindow.contentView = currentWindow.contentView
+         newWindow.titleVisibility = .hidden
+         newWindow.styleMask.remove([.resizable, .titled])
+         newWindow.level = .floating
+         newWindow.setFrame(NSScreen.main?.frame ?? .zero, display: true)
+         newWindow.makeKeyAndOrderFront(nil)
+
+         currentWindow.close()
       }
       return view
    }
    
    func updateNSView(_ nsView: NSView, context: Context) {}
+
+   final class ResponsiveWindow: NSWindow {
+      override var canBecomeKey: Bool { true }
+      override var canBecomeMain: Bool { true }
+      override var acceptsFirstResponder: Bool { true }
+   }
 }
